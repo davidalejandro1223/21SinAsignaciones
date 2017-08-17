264d712866d60882865b52995834844eae44f4a8
David Aparicio <davidalejandro1223@hotmail.com>
2017-08-17 13:59:50 -0500

    Implementados los casos donde se gana y se pierde, falta el manejo de las A

--- 21.py
+++ 21.py
@@ -5,7 +5,7 @@ def mazo():
 
 def valor_carta(carta):
 	if carta[0] == "A":
-		return 1
+		return 11
 	elif carta[0] in ["J","Q","K"]:
 		return 10
 	else:
@@ -17,15 +17,41 @@ def valor_mano(mano):
 	return valor_carta(mano[0]) + valor_mano(mano[1:])
 
 
-def jugar(mazo,manoJugador,manoCasa):
+def jugar(mazo,manoJugador,manoCasa,plantoJug):
 	print valor_mano(manoCasa)
 	print manoCasa
 	print valor_mano(manoJugador)
 	print manoJugador
 	if mazo!=[] and valor_mano(manoJugador)<= 21 and valor_mano(manoCasa)<= 21:
 		if(manoJugador == []):
-			return jugar(mazo[4:],manoJugador + mazo[:2], manoCasa + mazo[2:4])	
-		return jugar(mazo[2:],manoJugador + [mazo[0]], manoCasa + [mazo[1]])
+			return jugar(mazo[4:],manoJugador + mazo[:2], manoCasa + mazo[2:4], False)
+		if(plantoJug == False):
+			if(input("Pide carta?")==1):
+				return jugar(mazo[1:],manoJugador + [mazo[0]], manoCasa, False)
+			else:
+				if(valor_mano(manoCasa)<18):
+					return jugar(mazo[1:],manoJugador, manoCasa + [mazo[0]], True)
+				else:
+					if(valor_mano(manoCasa)>=valor_mano(manoJugador)):
+						print "Gano la casa"
+					else:
+						print "Gano el jugador"
+		else:
+			if(valor_mano(manoCasa)<18):
+				return jugar(mazo[1:],manoJugador, manoCasa + [mazo[0]], True)
+			else:
+				if(valor_mano(manoCasa)>=valor_mano(manoJugador)):
+					print "Gano la casa"
+				else:
+					print "Gano el jugador"
+
+	elif(valor_mano(manoCasa)>=18  or valor_mano(manoJugador)>21):
+		if((valor_mano(manoCasa)>=valor_mano(manoJugador) and valor_mano(manoCasa)<=21) or valor_mano(manoJugador)>21):
+			print "Gano la casa"
+		else:
+			print "Gano el jugador"
 
 
-jugar(mazo(),[],[])
+
+
+jugar(mazo(),[],[],False)
